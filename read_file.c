de71c464ac49f4b2ae8f9cc3668e82c7a592e2ca
Rana Elgheiti <relgheit@c2r9s4.42prague.com>
2025-03-04 15:10:51 +0100

    work in progress

--- read_file.c
+++ read_file.c
@@ -6,48 +6,73 @@
 /*   By: relgheit <relgheit@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/04 08:34:43 by relgheit          #+#    #+#             */
-/*   Updated: 2025/03/04 13:26:58 by relgheit         ###   ########.fr       */
+/*   Updated: 2025/03/04 15:10:01 by relgheit         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fdf.h"
 
-void	alloc_matrix(int **matrix, int x, int y)
+int	**alloc_matrix(int x, int y)
 {
 	int i;
+	int **matrix;
 
 	i = 0;
 	matrix = (int **)malloc(sizeof(int *) * y);
 	if (!matrix)
-		return ;
+		exit(EXIT_FAILURE);
 	while (i < y)
 	{
 		matrix[i] = (int *)malloc(sizeof(int) * x);
 		if (!matrix[i])
-			return ;
+			exit(EXIT_FAILURE);
 		i++;
 	}
-
+	return (matrix);
 }
 
-int	**fill_matrix(char *line, int y)
+// int	**fill_matrix(char *line, int y)
+// {
+// 	int	**matrix;
+// 	// char **temp;
+// 	// int	x;
+// 	// int i;
+	
+// 	// i = 0;
+// 	// x = 0;
+// 	// matrix = NULL;
+// 	// temp = ft_split(line, ' ');
+	
+// 	// while (temp[x])
+// 	// 	x++;
+// 	// // alloc_matrix(matrix, x, y);
+// 	ft_printf("line is : %s\n", line);
+	
+// 	return (matrix);
+// }
+
+int	**get_size(int fd, char *file)
 {
-	int	**matrix;
+	int x;
+	int y;
+	char *line;
 	char **temp;
-	int	x;
-	int i;
+	int **matrix;
 	
-	i = 0;
 	x = 0;
-	matrix = NULL;
+	y = 0;
+	line = NULL;
+	temp = NULL;
+	while ((line = get_next_line(fd)) != NULL)
+		y++;
+	fd = open(file, O_RDONLY);
+	line = get_next_line(fd);
 	temp = ft_split(line, ' ');
-	ft_printf("TEST");
-	
 	while (temp[x])
 		x++;
-	alloc_matrix(matrix, x, y);
-	ft_printf("line is : %s\n", line);
-	
+	ft_printf("x: %d\n", x);
+	ft_printf("y: %d\n", y);
+	matrix = alloc_matrix(x, y);
 	return (matrix);
 }
 
@@ -56,7 +81,7 @@ void    read_file(char *file)
 	int		fd;
 	char	*line;
 	int		**matrix;
-	int		y = 0;
+	// int		y = 0;
 
 	line = NULL;
 	fd = open(file, O_RDONLY);
@@ -65,14 +90,15 @@ void    read_file(char *file)
 		ft_printf("error opening file\n");
 		return ;
     }
-	while ((line = get_next_line(fd)) != NULL)
-		y++;
-	line = NULL;
-	ft_printf("y: %d\n", y);
-	while ((line = get_next_line(fd)) != NULL)
-	{
-		ft_printf("TEST");
-		matrix = fill_matrix(line, y);
-	}
+	// while ((line = get_next_line(fd)) != NULL)
+	// 	y++;
+	// line = NULL;
+	// ft_printf("y: %d\n", y);
+	// fd = open(file, O_RDONLY);
+	matrix = get_size(fd, file);
+	// while ((line = get_next_line(fd)) != NULL)
+	// {
+	// 	matrix = fill_matrix(line, y);
+	// }
 	close(fd);
 }
